58b3c6ed6482798bd27c80e28e19c83521ba4fae
marceldubach <marcel.dubach@epfl.ch>
2021-05-17 12:56:05 +0200

    generate objects

---
+++ main/modelling/generate_object_set.m
@@ -0,0 +1,42 @@
+function generate_object_set(nb_obj_per_set)
+
+
+% generate spheres
+par_sph_10.radius = 10;
+par_sph_15.radius = 15;
+par_sph_20.radius = 20;
+par_sph_25.radius = 25;
+par_sph_30.radius = 30;
+
+sph_10 = create_object_list('sph',par_sph_10, nb_obj_per_set);
+sph_15 = create_object_list('sph',par_sph_15, nb_obj_per_set);
+sph_20 = create_object_list('sph',par_sph_20, nb_obj_per_set);
+sph_25 = create_object_list('sph',par_sph_25, nb_obj_per_set);
+sph_30 = create_object_list('sph',par_sph_30, nb_obj_per_set);
+
+
+
+% generate cylinders
+par_cyl_R10H30.radius = 10; par_cyl_R10H30.height = 30;
+par_cyl_R15H30.radius = 15; par_cyl_R15H30.height = 30;
+par_cyl_R10H40.radius = 10; par_cyl_R10H40.height = 40;
+par_cyl_R15H40.radius = 15; par_cyl_R15H40.height = 40;
+par_cyl_R15H50.radius = 15; par_cyl_R15H50.height = 50;
+
+cyl_R10H30 = create_object_list('cyl',par_cyl_R10H30, nb_obj_per_set);
+cyl_R15H30 = create_object_list('cyl',par_cyl_R15H30, nb_obj_per_set);
+cyl_R10H40 = create_object_list('cyl',par_cyl_R10H40, nb_obj_per_set);
+cyl_R15H40 = create_object_list('cyl',par_cyl_R15H40, nb_obj_per_set);
+cyl_R15H50 = create_object_list('cyl',par_cyl_R15H50, nb_obj_per_set);
+
+% generate composite objects
+par_comp_1.radius = 10; par_comp_1.height = 30;
+par_comp_1.sphereCenter = [0,0,25;0,0,-25];
+par_comp_1.sphereRadius = [15,15];
+
+comp1 = create_object_list('comp',par_comp_1, nb_obj_per_set);
+
+save('objects.mat','sph_10','sph_15','sph_20','sph_25','sph_30',...
+                    'cyl_R10H30','cyl_R15H30','cyl_R10H40','cyl_R15H40','cyl_R15H50',...
+                    'comp1')
+end
--- main/reset_symbolic_functions.m
+++ main/reset_symbolic_functions.m
@@ -2,15 +2,41 @@ function reset_symbolic_functions()
 % function to delete constraint and objective functions built from symbolic
 % expressions (should be called anytime the problem definition changes)
 
-if isfile('../database/symbolic_functions/objfun.m')
-    % if one file exists, delete all
-    delete ../database/symbolic_functions/nonl_ceq.m
-    delete ../database/symbolic_functions/nonl_ceq_grad.m
+functions_exist = false;
+
+% if one file exists, delete all
+if isfile('../database/symbolic_functions/nonl_c.m')
     delete ../database/symbolic_functions/nonl_c.m
+    functions_exist = true;
+end
+if isfile('../database/symbolic_functions/nonl_c_grad.m')
     delete ../database/symbolic_functions/nonl_c_grad.m
+    functions_exist = true;
+end
+
+% nonlinear equality constraints
+if isfile('../database/symbolic_functions/nonl_ceq.m')
+    delete ../database/symbolic_functions/nonl_ceq.m
+    functions_exist = true;
+end
+if isfile('../database/symbolic_functions/nonl_ceq_grad.m')
+    delete ../database/symbolic_functions/nonl_ceq_grad.m
+    functions_exist = true;
+end
+
+% objective function
+if isfile('../database/symbolic_functions/objfun.m')
     delete ../database/symbolic_functions/objfun.m
+    functions_exist = true;
+end
+if isfile('../database/symbolic_functions/objfun_grad.m')
     delete ../database/symbolic_functions/objfun_grad.m
+    functions_exist = true;
+end
+
+if functions_exist
+    fprintf('\nDeleted symbolic functions\n')
 else
-    fprintf('No symbolic functions to delete')
+    fprintf('\nNo symbolic functions to delete\n')
 end
 
