68a67b7b3899cc15b219c069fabac4f0be1b7c37
marceldubach <marcel.dubach@epfl.ch>
2021-05-31 16:53:54 +0200

    corrected experiment analysis

--- main/analyze_experiments.m
+++ main/analyze_experiments.m
@@ -4,44 +4,62 @@
 clear all;
 close all;
 %% load the data as tables
-T_opt = readtable('../results/experiment_results_optim.csv');
-T_exp = readtable('../results/experiment_results_exp.csv');
+T_opt = readtable('../database/robot_experiment/analysis/experiment_results_optim.csv');
+T_exp = readtable('../database/robot_experiment/analysis/experiment_results_exp.csv');
 
 % unused: compute bias separately
 % hand_center = [0.931719,-1.142540, 1.365805]; % hand center in meters
 
-hand_center_opt = [0,-29.4975,0];   % hand center in Matlab [mm]
+hand_ctr_sim = [0,-29.4975,0];   % hand center in Matlab [mm]
 
-rot_x = 1000*[1,0,0;0,0,1;0,-1,0];  % matrix to rotate from MC-frame to Matlab frame
-                                    % (converts also to [mm]
+rot_x = 1000*[1,0,0;0,0,1;0,-1,0];  % transformation  MCS to Simulation
+                                    % (convert [m] to [mm])
+% MCS -> S 
+% y in S = z in MCS
+% z in S = -y in MCS
 
+%%% load logfiles
+nb_experience = 21;
 
-%%% compute the bias
+exp_data  = T_exp(1:nb_experience,:);
+obj_ctr_mc = table2array(exp_data(:,{'x','y','z'}));
 
-% retrieve data of object centers for the first 2 experiments
-exp_data  = T_exp(1:2,:);
-center_mc = table2array(exp_data(1:2,{'x','y','z'}));
+opt_data = T_opt(1:nb_experience,:);
+obj_ctr_sim = table2array(opt_data(:,{'x','y','z'}));
+obj_ctr_sim = obj_ctr_sim - hand_ctr_sim;
 
-opt_data = T_opt(1:2,:);
-center_opt = table2array(opt_data(1:2,{'x','y','z'}));
+%%% transform motion capture data to simulation frame
 
-% transform to simulation frame
-rel_center_mc = (rot_x * center_mc.').';
+% object center according to mc frame
+obj_ctr = (rot_x * (obj_ctr_mc.')).'; % size ( N x 3 )
 
-% compute the bias
-bias =  mean(center_opt - rel_center_mc,1);
+% compute the offset 
+offset = mean(obj_ctr_sim,1) - mean(obj_ctr,1);
 
+% add this bias to the obj_ctr;
+obj_ctr = obj_ctr + offset; % correct for the wrong origin
 
+% compute the array for c1 and c2 (for orientation)
+c1_array = table2array(exp_data(:,{'c1_1','c1_2','c1_3'}));
+c2_array = table2array(exp_data(:,{'c2_1','c2_2','c2_3'}));
+
+c1_array = (rot_x * c1_array.').' + offset;
+c2_array = (rot_x * c2_array.').' + offset;
+
+% retrieve orientations
+axis_sim = table2array(opt_data(:,{'quat2','quat3','quat4'}));
 %%% analyze the data
-nb_experience = 19
-is_sph = [ones(1,7),zeros(1,12)];
-is_cyl = [zeros(1,7),ones(1,8),zeros(1,4)];
-is_comp = [zeros(1,15),ones(1,4)];
-comp_distance = ones(1,21); % compute distance on all the samples
+nb_experience = 21;
+is_sph = [ones(1,9),zeros(1,12)];
+is_cyl = [zeros(1,9),ones(1,8),zeros(1,4)];
+is_comp = [zeros(1,17),ones(1,4)];
 
 % initialize all arrays
-pos_variables = {'x','y','z'};
 joint_angles = {'q1','q2','q3','q4','q5','q6','q7','q8','q9','q10','q11','q12','q13','q14','q15','q16'};
+
+% construct an array containting info about contacting fingers
+finger_data = table2array(exp_data(:,{'finger1','finger2'}));
+
 % compute joint angle variations only for the active fingers
 % note that thumb has index q13 - q16
 F = [{'q13','q14','q15','q16'};.... % F1
@@ -49,122 +67,145 @@ F = [{'q13','q14','q15','q16'};.... % F1
     {'q5','q6','q7','q8'};...       % F3    
     {'q9','q10','q11','q12'}];...    % F4
     
-
 % center position of all objects
-sph_ctr_pos = []; 
+sph_ctr_pos_err = []; 
 sph_ctr_dist = [];
 sph_err_JA = [];
 sph_err_axis = [];
 
-cyl_ctr_pos = [];
+cyl_ctr_pos_err = [];
 cyl_ctr_dist = [];
 cyl_err_JA = [];
 cyl_err_axis = [];
 
-comp_ctr_pos = [];
+comp_ctr_pos_err = [];
 comp_ctr_dist = [];
 comp_err_JA = [];
 comp_err_axis = [];
 
 
-center_opt = [];
-center_exp = [];
+center_opt_arr = [];
+center_exp_arr = [];
 
+hand_center_predictions = [];
 % loop over all 20 experiences
 for i=1:nb_experience
     % center position in motion capture frame
-    exp_data  = T_exp(i,:);
-    pos_mc = table2array(exp_data(:,pos_variables)); % MC center position
-    pos = (rot_x * pos_mc.').' + bias;     % convert to simulation frame
+    oc_mc = obj_ctr(i,:);      % obj_ctr of recorded data
+    oc_sim = obj_ctr_sim(i,:); % obj_ctr of simulation
     
-    % center position in optimization (in [m])
-    opt_data = T_opt(i,:);
-    pos_sim = table2array(opt_data(:,pos_variables))-hand_center_opt;
+    pos_diff = oc_sim - oc_mc; % position difference simulation w.r.t. MC
+    d_pos_diff = norm(pos_diff,2);
     
+    % retrieve joing angles
+    finger_pair = finger_data(i,:);
+    finger_pair = finger_pair(finger_pair ~= 0); % remove palm
+    if length(finger_pair) == 2
+        grasp_joint_angles = [F(finger_pair(1),:), F(finger_pair(2),:)];  
+    else
+        grasp_joint_angles = [F(finger_pair,:)];
+    end
+    joint_angles_rec = table2array(exp_data(i,grasp_joint_angles));
+    joint_angles_sim = table2array(opt_data(i,grasp_joint_angles));
     
-    %%% joint angle comparison
-    fingers = table2array(exp_data(:,{'finger1','finger2'}));
-    grasp_joint_angles = [];
-    for j=1:2               % extract joint angle values of active fingers
-        if fingers(j)~=0
-            grasp_joint_angles = [grasp_joint_angles, F(fingers(j),:)];
-        end
+    % standard deviation in JA error
+    err_JA = std(joint_angles_rec - joint_angles_sim);    
+    
+    % computation of the orientation (only for non-spherical objects)
+    if ~is_sph(i)
+        c1 = c1_array(i,:).';
+        c2 = c2_array(i,:).';
+        
+        ax_mc = (c1-c2)/norm(c1-c2, 2); % object axis of recorded data
+        
+        % object axis of simulation (normalize it)
+        ax_sim = axis_sim(i,:).' / norm(axis_sim(i,:),2); 
+
+        axis_err = 1 - abs(dot(ax_mc,ax_sim));
     end
     
-    % compute standard deviation in active joint angles
-    robot_JA = table2array(exp_data(:,grasp_joint_angles));
-    sim_JA = table2array(opt_data(:,grasp_joint_angles));
-    err_JA = std(robot_JA - sim_JA);
-    
-    
-    
-    %%% computation of the orientation
-    c1 = table2array(exp_data(:,{'c1_1','c1_2','c1_3'}))';
-    c1 = (rot_x * c1) + bias';
-    c2 = table2array(exp_data(:,{'c2_1','c2_2','c2_3'}))';
-    c2 = (rot_x * c2) + bias';
-    
-    axis_exp = (c1-c2)/norm(c1-c2, 2);
-    
-    axis_sim = table2array(opt_data(:,{'quat2','quat3','quat4'}))';
-    
-    axis_err = 1 - abs(dot(axis_exp,axis_sim));
-    
     if is_sph(i)
-        sph_ctr_pos(end+1,:) = pos - pos_sim;
-        sph_ctr_dist(end+1) = norm(pos_sim,2);
+        sph_ctr_pos_err(end+1,:) = pos_diff;
+        sph_ctr_dist(end+1) = d_pos_diff;
         sph_err_JA(end+1) = err_JA;
     end
     
     if is_cyl(i)
-        cyl_ctr_pos(end+1,:) = pos - pos_sim;
-        cyl_ctr_dist(end+1) = norm(pos - pos_sim,2);
+        cyl_ctr_pos_err(end+1,:) = pos_diff;
+        cyl_ctr_dist(end+1) = d_pos_diff;
         cyl_err_JA(end+1) = err_JA;
         cyl_err_axis(end+1) = axis_err;
     end
     
     if is_comp(i)
-       comp_ctr_pos(end+1,:) = pos - pos_sim;
-       comp_ctr_dist(end+1) = norm(pos - pos_sim,2);
+       comp_ctr_pos_err(end+1,:) = pos_diff;
+       comp_ctr_dist(end+1) = d_pos_diff;
        comp_err_JA(end+1) = err_JA;
        comp_err_axis(end+1) = axis_err;
     end    
     
-    center_opt(end+1,:) = pos_sim;
-    center_exp(end+1,:) = pos;
 end
 
 
+% PLOT POSITION ERROR ALONG EACH AXIS
+figure
+ax = gca;
+ax.FontSize = 20; 
+subplot(311),scatter(1:size(sph_ctr_dist,2),sph_ctr_pos_err(:,1),'*');
+xlim([1,21])
+hold on
+scatter(10:9+size(cyl_ctr_dist,2),cyl_ctr_pos_err(:,2),'*');
+scatter(18:17+size(comp_ctr_dist,2),comp_ctr_pos_err(:,2),'*');
+ylabel('$e_x$','Interpreter','latex','Fontsize',20);
+hold on
+subplot(312),scatter(1:size(sph_ctr_dist,2),sph_ctr_pos_err(:,2),'*');
+xlim([1,21])
+hold on
+scatter(10:9+size(cyl_ctr_dist,2),cyl_ctr_pos_err(:,2),'*');
+scatter(18:17+size(comp_ctr_dist,2),comp_ctr_pos_err(:,2),'*');
+ylabel('$e_y$','Interpreter','latex','Fontsize',20);
+subplot(313),scatter(1:size(sph_ctr_dist,2),sph_ctr_pos_err(:,3),'*');
+hold on
+subplot(313),scatter(10:9+size(cyl_ctr_dist,2),cyl_ctr_pos_err(:,3),'*');
+subplot(313),scatter(18:17+size(comp_ctr_dist,2),comp_ctr_pos_err(:,3),'*');
+xlim([1,21])
+ylabel('$e_z$','Interpreter','latex','Fontsize',20);
+xlabel('Experience','Fontsize',20)
+sgtitle('Position error','Fontsize',24)
+
 % DISTANCE BOXPLOTS
 figure
+ax = gca;
+ax.FontSize = 20; 
 subplot(1,3,1), boxplot(sph_ctr_dist)
-ylim([0,150])
-ylabel('Distance to simulation [mm]')
-xlabel('Sphere objects')
+ylim([0,100])
+ylabel('Distance to simulation [mm]','Fontsize',16')
+xlabel('Sphere objects','Fontsize',16')
 subplot(1,3,2), boxplot(cyl_ctr_dist)
-ylim([0,150])
-xlabel('Cylinder objects')
+ylim([0,100])
+xlabel('Cylinder objects','Fontsize',16')
 subplot(1,3,3), boxplot(comp_ctr_dist)
-ylim([0,150])
-xlabel('Composite objects')
-sgtitle('Distance deviations compared to simulation', 'FontWeight','bold')
+ylim([0,100])
+xlabel('Composite objects','Fontsize',16')
 saveas(gcf, '../results/distance_deviation.fig')
 saveas(gcf, '../results/distance_deviation.png')
 
 % JOINT ANGLE BOXPLOTS
 figure
+ax = gca;
+ax.FontSize = 16; 
+
 subplot(1,3,1), boxplot(sph_err_JA)
 ylim([0,0.1])
-
-ylabel('[rad]')
-xlabel('Sphere objects')
+ylabel('Joint angle deviation [rad]','Fontsize',16)
+xlabel('Sphere objects','Fontsize',16)
 subplot(1,3,2), boxplot(cyl_err_JA)
 ylim([0,0.1])
-xlabel('Cylinder objects')
+xlabel('Cylinder objects','Fontsize',16)
 subplot(1,3,3), boxplot(comp_err_JA)
 ylim([0,0.1])
-xlabel('Composite objects')
-sgtitle('Standard deviation in joint angles', 'FontWeight','bold')
+xlabel('Composite objects','Fontsize',16')
+%sgtitle('Standard deviation in joint angles', 'FontWeight','bold','Fontsize',20)
 
 saveas(gcf, '../results/joint_angle_deviation.fig')
 saveas(gcf, '../results/joint_angle_deviation.png')
@@ -172,47 +213,46 @@ saveas(gcf, '../results/joint_angle_deviation.png')
 % ORIENTATION ANGLE BOXPLOTS
 figure
 ylabel('[rad]')
-xlabel('Sphere objects')
 subplot(1,2,1), boxplot(cyl_err_axis)
 ylim([0,1])
-xlabel('Cylinder objects')
+xlabel('Cylinder objects','Fontsize',16')
+ylabel('Orientation deviation','Fontsize',16')
 subplot(1,2,2), boxplot(comp_err_axis)
 ylim([0,1])
-xlabel('Composite objects')
-sgtitle('Deviation in orientation', 'FontWeight','bold')
+xlabel('Composite objects','Fontsize',16')
 saveas(gcf, '../results/orientation_deviation.fig')
 saveas(gcf, '../results/orientation_deviation.png')
-
-% plot all distances
-% figure
-% subplot(1,3,1), plot(sph_ctr_pos)
-% subplot(1,3,2), plot(cyl_ctr_pos)
-% subplot(1,3,3), plot(comp_ctr_pos)
-
-% figure
-% subplot(1,3,1),plot(center_opt(:,1))
-% hold on
-% plot(center_exp(:,1))
-% xlim([1 21])
-% xlabel('Experience')
-% ylabel('Distance [mm]')
-% legend('OPT','EXP')
-% title('Position estimation: x-axis')
 % 
-% subplot(1,3,2),plot(center_opt(:,2))
-% hold on
-% plot(center_exp(:,2))
-% xlim([1 21])
-% xlabel('Experience')
-% ylabel('Distance [mm]')
-% legend('OPT','EXP')
-% title('Position estimation: y-axis')
+% % plot all distances
+% % figure
+% % subplot(1,3,1), plot(sph_ctr_pos)
+% % subplot(1,3,2), plot(cyl_ctr_pos)
+% % subplot(1,3,3), plot(comp_ctr_pos)
 % 
-% subplot(1,3,3),plot(center_opt(:,3))
-% hold on
-% plot(center_exp(:,3))
-% xlim([1 21])
-% xlabel('Experience')
-% ylabel('Distance [mm]')
-% legend('OPT','EXP')
-% title('Position estimation: z-axis')
+% % figure
+% % subplot(1,3,1),plot(center_opt(:,1))
+% % hold on
+% % plot(center_exp(:,1))
+% % xlim([1 21])
+% % xlabel('Experience')
+% % ylabel('Distance [mm]')
+% % legend('OPT','EXP')
+% % title('Position estimation: x-axis')
+% % 
+% % subplot(1,3,2),plot(center_opt(:,2))
+% % hold on
+% % plot(center_exp(:,2))
+% % xlim([1 21])
+% % xlabel('Experience')
+% % ylabel('Distance [mm]')
+% % legend('OPT','EXP')
+% % title('Position estimation: y-axis')
+% % 
+% % subplot(1,3,3),plot(center_opt(:,3))
+% % hold on
+% % plot(center_exp(:,3))
+% % xlim([1 21])
+% % xlabel('Experience')
+% % ylabel('Distance [mm]')
+% % legend('OPT','EXP')
+% % title('Position estimation: z-axis')
---
+++ main/plot_os_results.m
@@ -0,0 +1,72 @@
+% analyze data of opposition space
+
+success_map = nan(13);
+
+for k=1:numel(os_list)
+    FL1 = os_list{k}.os_info{1};
+    FL2 = os_list{k}.os_info{2};
+    
+    finger1 = FL1(1);
+    link1 = FL1(2);
+    
+    finger2 = FL2(1);
+    link2 = FL2(2);
+    
+    % find the correct column
+    if finger1 == 0
+        i = 13;
+    else
+        if finger1 == 1
+            i = link1-1;
+        else
+            i = 3*(finger1-1) + link1;
+        end
+    end
+    
+    if finger2 == 0
+        j = 13;
+    else
+        if finger2 == 1
+            j = link2-1;
+        else
+            j = 3*(finger2-1) + link2;
+        end
+    end
+    fprintf("%d \t  %d\n", i, j);
+    
+    success = 0;
+    for n=1:numel(successful_os)
+        tmp_os1 = successful_os{n}{1};
+        tmp_os2 = successful_os{n}{2};
+        tmp_F1 = tmp_os1(1)
+        tmp_L1 = tmp_os1(2);
+        
+        tmp_F2 = tmp_os2(1);
+        tmp_L2 = tmp_os2(2);
+        if (tmp_F1 == finger1) && (tmp_L1 == link1)
+            if (tmp_F2 == finger2) && (tmp_L2 == link2)
+                success = 1;
+            end
+        end
+    end
+    success_map(i,j) = success;
+            
+end
+
+category_list = {'1-2','1-3','1-4','2-2','2-3','2-4','3-2','3-3','3-4','4-2','4-3','4-4','P'}
+
+col_bar = [1,0,0;0.8,0,0.2;0.6,0,0.4;0.4,0,0.6;0.2,0,0.8;0,0,1];
+
+figure;
+heatmap(success_map,...
+    'XData',category_list,...
+    'YData',category_list,...
+    'MissingDataColor', 'w',...
+    'Colormap',col_bar,...
+    'ColorLimits',[0 1],...
+    'CellLabelColor','none');
+title(['Successful grasps (object radius ', num2str(15),')']);
+xlabel('Finger-Link pair');
+ylabel('Finger-Link pair');
+set(gca, 'FontSize', 14);
+    
